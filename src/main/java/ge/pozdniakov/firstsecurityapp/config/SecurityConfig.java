3131b6cbdd8d8fa82c41efd5de1481edc10cc100
Pozdnyakov Anton <annihilator61@mail.ru>
2023-01-09 23:13:23 +0300

    add authorization with roles admin and user

--- src/main/java/ge/pozdniakov/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/ge/pozdniakov/firstsecurityapp/config/SecurityConfig.java
@@ -32,10 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
          //конфигурируем сам Spring Security
         //конфигурируем авторизацию
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().hasAnyRole("USER","ADMIN")
                 .and()
                 .formLogin()
                 .loginPage("/auth/login")
